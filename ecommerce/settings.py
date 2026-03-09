8a346adfce1960cb0a069a789f35f96bbb44b061
Shivani Sharma <shivanibhardwaj585@gmail.com>
2026-03-09 12:20:16 +0530

    deploy setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wi1#y$z2hayf!8-o%x*t7_erw)@t-f&_$h--f+$z3c$t@4c-^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
